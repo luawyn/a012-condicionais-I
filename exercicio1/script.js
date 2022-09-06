75581c12b091339bd8b22fcf689ca98aad474245
Luana Trevizani <luanaftrevizani@gmail.com>
2022-09-06 18:43:48 -0300

    exercicios corrigidos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,5 +1,10 @@
-const num = //seu numero aqui
+const num = parseInt(prompt('Digite um número: '))
 
-if(){
-    console.log("sua mensagem aqui");
+if (num % 2 === 0) {
+  console.log('O número é par')
+} else {
+  console.log('O número é ímpar')
 }
+// # Exercício 1
+
+// Crie um código que receba uma const numérica qualquer. Crie um `if` para verificar se o número guardado na const é **par**. Caso seja, imprima no console a mensagem: "o número é par".
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,45 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+const nome = prompt('Digite seu nome: ')
+let idade = parseInt(prompt('Digite sua idade: '))
+let terminouEnsinoMedio = confirm('Terminou o ensino médio?')
+let isCursandoFaculdade = confirm('Cursando alguma faculdade?')
 
-if(){
-
-}else{
-	
+if (idade >= 18) {
+  console.log(nome + ', você é maior de idade!')
+} else {
+  console.log(nome + ', você é menor de idade!')
 }
+if (terminouEnsinoMedio === true) {
+  console.log(nome + ', você terminou o ensino médio!')
+} else {
+  console.log(nome + ', você não terminou o ensino médio!')
+}
+if (isCursandoFaculdade === true) {
+  console.log(nome + ', você está cursando uma faculdade!')
+} else {
+  console.log(nome + ', você não está cursando uma faculdade!')
+}
+
+// # Exercício 2
+
+// Escreva um código que receba três valores. Uma idade, um booleano que responda se a pessoa terminou ou não o ensino médio, e um booleano que responda se a pessoa está cursando alguma faculdade.
+
+// Crie um `if` para cada variável, checando as seguintes afirmacões:
+// - Se a pessoa tem 18 anos ou mais;
+// - Se a pessoa terminou o ensino médio;
+// - Se a pessoa NÃO está cursando alguma faculdade;
+
+// Crie um `if` que imprima que a afirmacão é verdadeira, e um `else` para imprimir que a afirmacão não é verdadeira.
+
+// <details>
+
+// <summary>Exemplo</summary>
+
+// Caso a primeira pessoa tenha idade >=18:
+
+// <b>"A pessoa é maior de idade"</b>
+
+// e caso não seja:
+
+// <b>"A pessoa é menor de idade"</b>
+
+// </details>
